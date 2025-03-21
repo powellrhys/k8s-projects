19f265dec375adbb16ca7dfb470efc1f2fd17902
rhys powell <rhys.powell2@btinternet.com>
2025-03-21 18:17:50 +0000

    duplciate code put into individual scripts

--- 02_Database_App/deploy_application.ps1
+++ 02_Database_App/deploy_application.ps1
@@ -1,34 +1,17 @@
 # Define script variables
 $container_name = 'k8s-simple-database-app'
+$sql_username = 'sa'
+$sql_password = 'YourPassword123'
+$sql_db = 'mydatabase'
+$sql_table = "users"
 
-$minikubeStatus = minikube status 2>$null
+# Configure minikube to run kubernetes locally
+.\..\scripts\configure-minikube.ps1
 
-if ($minikubeStatus -match "Host: Running") {
-    Write-Output "Minikube is already running."
-} elseif ($minikubeStatus -match "Profile .* not found") {
-    Write-Output "No Minikube profile found. Starting a new Minikube cluster..."
-    minikube start
-} else {
-    Write-Output "Minikube is NOT running. Starting now..."
-    minikube start
-}
-Write-Host "Minikube has been successfully configured`n"
-
-# Change docker build context to point towards minikube's internal docker daemon
-Write-Host "Switching docker build context..."
-minikube docker-env | Invoke-Expression
-Write-Host "Minikube internal docker daemon configured`n"
-
-# Build docker container
-Write-Host "Building docker $($container_name)...`n"
-try {
-    cd frontend
-    docker build -t $container_name .
-    cd ..
-    Write-Host "`nContainer $($container_name) successfully built`n"
-} catch {
-    Write-Host "`nFailed to build $($container_name)`n"
-}
+# Build Container within minikube context
+cd frontend
+.\..\..\scripts\build-container.ps1 -container_name $container_name
+cd ..
 
 # Deploy application
 Write-Host "Deploying application..."
@@ -38,60 +21,46 @@ kubectl apply -f config/sqlserver-deployment.yml
 kubectl apply -f config/streamlit-deployment.yml
 Write-Host "Application deployed`n"
 
-Write-Output "Waiting for all pods to be in 'Running' state..."
+# Ensure all pods are up and running before proceeding
+.\..\scripts\monitor-pod-status.ps1
 
-do {
-    # Get the status of all pods in the default namespace
-    $podStatus = kubectl get pods --no-headers | ForEach-Object { ($_ -split "\s+")[2] }
-
-    # Assume all pods are running unless we find one that isn't
-    $allRunning = $true
-
-    foreach ($status in $podStatus) {
-        if ($status -ne "Running") {
-            $allRunning = $false
-            break
-        }
-    }
-
-    # Wait and retry if not all pods are running
-    if (-not $allRunning) {
-        Start-Sleep -Seconds 5
-        Write-Output "Still waiting for pods to be ready..."
-        kubectl get pods
-    }
-
-} while (-not $allRunning)
-
-Write-Output "All pods are running!`n"
-
-
+# Forward database entrypoint to localhost
 Write-Host "Port forwarding SQL Server service..."
 Start-Job -ScriptBlock {
     kubectl port-forward svc/sqlserver-service 1433:1433
 }
-
-Start-Sleep -Seconds 5  # Wait for the port forward to establish
+Start-Sleep -Seconds 5
 
 Write-Host "Logging into SQL Server"
 
-# Connect using localhost now that port-forward is running
-sqlcmd -S localhost,1433 -U sa -P 'YourPassword123' -Q "SELECT name FROM sys.databases"
-
 # Create database if it doesn't exist
-sqlcmd -S localhost,1433 -U sa -P 'YourPassword123' -Q "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'mydatabase') CREATE DATABASE mydatabase"
+Write-Host "Configuring database..."
+$sql_query = @"
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = '$sql_db')
+    CREATE DATABASE $sql_db
+"@
+sqlcmd -S localhost,1433 -U $sql_username -P $sql_password -Q $sql_query
 
-# Create table
-sqlcmd -S localhost,1433 -U sa -P 'YourPassword123' -d mydatabase -Q "CREATE TABLE users (id INT PRIMARY KEY IDENTITY, name NVARCHAR(100), email NVARCHAR(100) UNIQUE)"
+# Create table if it doesn't exist
+Write-Host "Configure SQL table..."
+$table_query = @"
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = '$sql_table' AND schema_id = SCHEMA_ID('dbo'))
+    CREATE TABLE dbo.$sql_table (
+        id INT PRIMARY KEY IDENTITY,
+        name NVARCHAR(100),
+        email NVARCHAR(100) UNIQUE
+    )
+"@
+sqlcmd -S localhost,1433 -U $sql_username -P $sql_password -d $sql_db -Q $table_query
 
-# Insert data
-sqlcmd -S localhost,1433 -U sa -P 'YourPassword123' -d mydatabase -Q "INSERT INTO users (name, email) VALUES ('Rhys', 'rhys@example.com');"
-
-# Verify table exists
-sqlcmd -S localhost,1433 -U sa -P 'YourPassword123' -d mydatabase -Q "SELECT name FROM sys.tables WHERE name = 'users'"
-
-# Retrieve data
-sqlcmd -S localhost,1433 -U sa -P 'YourPassword123' -d mydatabase -Q "SELECT * FROM users"
+# Define insert into table if data is currently there
+Write-Host "Inserting data into SQL table...`n"
+$insert_query = @"
+IF NOT EXISTS (SELECT 1 FROM dbo.$sql_table)
+    INSERT INTO dbo.$sql_table (name, email) 
+    VALUES ('John', 'John@example.com')
+"@
+sqlcmd -S localhost,1433 -U $sql_username -P $sql_password -d $sql_db -Q $insert_query
 
 # Report pods status
 Write-Host "Exposing application..."
